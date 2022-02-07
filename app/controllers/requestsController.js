c037e278590186334a88b2d290099f1df103ab3a
Tilen Barbirič <40025611+barbirict@users.noreply.github.com>
2022-02-08 00:52:19 +0100

    Begin TVDB request implementation  -Add requestsController.js - prototypes  -Install moviedb-promise

---
+++ app/controllers/requestsController.js
@@ -0,0 +1,37 @@
+
+
+exports.getOne = (req, res) => {
+
+}
+
+exports.getAll = (req, res) => {
+
+}
+
+exports.getDateRange = (req, res) => {
+
+}
+
+exports.getBySubId = (req, res) => {
+
+}
+
+exports.getByType = (req, res) => {
+
+}
+
+exports.createNew = (req, res) => {
+
+}
+
+exports.verify = (req, res) => {
+
+}
+
+exports.modifyId = (req, res) => {
+
+}
+
+exports.deleteId = (req, res) => {
+
+}
